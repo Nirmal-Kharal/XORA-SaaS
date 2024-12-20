67c50b468675f26ddb1678fd542ce0016f26202d
Nirmal-Kharal <nirmalkharal40@gmail.com>
2024-12-20 16:34:42 +0545

    feat:Faq/testimonials done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import FAQ from "./sections/FAQ";
 import Features from "./sections/Features";
 import Header from "./sections/Header";
 import Hero from "./sections/Hero";
 import Pricing from "./sections/Pricing";
+import Testimonials from "./sections/Testimonials";
 
 const App = () => {
   return (
@@ -10,6 +12,8 @@ const App = () => {
       <Hero/>
       <Features/>
       <Pricing />
+      <FAQ />
+      <Testimonials />
     </main>
   );
 };
